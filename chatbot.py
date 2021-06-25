5ceea995f490454dab6189e144a4e89b44aad569
李承澔 <chenghaolee@tdsc.com.tw>
2021-06-26 02:56:59 +0800

    finish articles countings

--- chatbot.py
+++ chatbot.py
@@ -3,12 +3,15 @@ import requests
 import json
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+import datetime
+
 
 class chatBot(Auth):
     def __init__(self):
         super().__init__()
         self._load_slack_token()
         self._client = WebClient(token=self._slack_token)
+        self._response = None
 
     def _load_slack_token(self):
         with open("config.json", "r") as f:
@@ -19,25 +22,38 @@ class chatBot(Auth):
     def check_db_exist(self):
         pass
 
-    def query_notion(self, query=None):
+    def query_notion(self):
         # if query==None
         #   query all
         # else
         #   do query
+        date = (datetime.datetime.now()-datetime.timedelta(days=2)
+                ).replace(microsecond=0).isoformat()
+        date = date[:11]+'18:00:00.000Z'
+        query = {
+            "filter": {
+                "property": "Last Edited Time",
+                "date": {
+                    "on_or_after": date
+                }
+            }
+        }
         if query:
             secret_key = self._secret_key
             header = {"Authorization": secret_key,
                       "Notion-Version": "2021-05-13"}
             response = requests.post(
-                self.base_url + self._database_id, headers=header, json=query)
-            if response.status_code==200:
-                return response.json()["results"]
+                self.base_url + self._database_id+ "/query", headers=header, json=query)
+            if response.status_code == 200:
+                self._response = len(response.json()["results"])
+                # print(response.json()["results"])
+                return self._response
             else:
                 pass
         else:
             pass
-    
-    def get_channel_id(self,channel_name="chatbot-test"):
+
+    def get_channel_id(self, channel_name="chatbot-test"):
         # channel_name = "chatbot-test"
         channel_id = None
         try:
@@ -49,25 +65,38 @@ class chatBot(Auth):
                     if channel["name"] == channel_name:
                         channel_id = channel["id"]
                         self.channel_id = channel_id
-                        #Print result
+                        # Print result
                         # print(f"Found conversation ID: {conversation_id}")
                         return self.channel_id
                 return -1
         except SlackApiError as e:
             print(f"Error: {e}")
             return -1
-    
-    def post_msg(self):
+
+    def post_msg(self, texts=None):
         try:
             # Call the conversations.list method using the WebClient
-            result = self._client.chat_postMessage(
-                channel=self.channel_id,
-                # blocks=blocks,
-                text="There is no new article updated today! You guys suck..."
-                # You could also use a blocks[] array to send richer content
-            )
-            # Print result, which includes information about the message (like TS)
-            print(result)
+            self.query_notion()
+            if self._response == 0:
+                result = self._client.chat_postMessage(
+                    channel=self.channel_id,
+                    # blocks=blocks,
+                    text="There is no new article updated today! You guys suck..."
+                    # You could also use a blocks[] array to send richer content
+                )
+                # Print result, which includes information about the message (like TS)
+                print(result)
+            else:
+                date = (datetime.datetime.now()-datetime.timedelta(days=1)
+                        ).replace(microsecond=0).isoformat()[:10]
+                result = self._client.chat_postMessage(
+                    channel=self.channel_id,
+                    # blocks=blocks,
+                    text="Total {} articles were updated on {}".format(self._response,date)
+                    # You could also use a blocks[] array to send richer content
+                )
+                # Print result, which includes information about the message (like TS)
+                print(result)
 
         except SlackApiError as e:
             print(f"Error: {e}")
